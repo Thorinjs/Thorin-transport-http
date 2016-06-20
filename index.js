bc2170999755d8983f6d8273a4843da4d23684bb
snupa <bunta.adrian@gmail.com>
2016-06-20 17:59:09 +0300

    added _handleIntentJson for projects that must support backwards compatibility with JSON structure.

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ module.exports = function init(thorin) {
     app = Symbol();
 
   class http extends thorin.Interface.Transport {
-    static publicName() { return "http"; }
+    static publicName() {
+      return "http";
+    }
 
     constructor() {
       super();
@@ -28,14 +30,15 @@ module.exports = function init(thorin) {
       this[config] = {};
       this[app] = null;
     }
+
     get app() {
-      if(!this[app]) return null;
+      if (!this[app]) return null;
       return this[app];
     }
 
     /*
-    * Initializes the transport with config.
-    * */
+     * Initializes the transport with config.
+     * */
     init(httpConfig) {
       this[config] = thorin.util.extend({
         debug: true,
@@ -55,53 +58,73 @@ module.exports = function init(thorin) {
         }
       }, httpConfig);
       this[app] = new ExpressApp(this[config], this._log.bind(this));
-      for(let i=0; i < this[middleware].length; i++) {
+      for (let i = 0; i < this[middleware].length; i++) {
         this[app]._addMiddleware(this[middleware][i]);
       }
       this[middleware] = [];
     }
 
     /*
-    * Checks if we trust a reverse proxy or not
-    * */
+     * Checks if we trust a reverse proxy or not
+     * */
     trustProxy() {
       return this[config].trustProxy;
     }
 
     /*
-    * Sets up the directory structure of the project.
-    * */
+     * Override the default API intent response structure. By default,
+     * we have a "type":{actionName}, "result": {object}.
+     * Note: this should not be used other than to offer backwards-compatibility
+     * */
+    _handleIntentJson(fn) {
+      if (!this.app) {
+        this._log("warn", 'handleIntentJson: app is not started yet.');
+        return false;
+      }
+      if (typeof fn !== 'function') {
+        this._log('error', 'handleIntentJson: callback is not a function');
+        return false;
+      }
+      this.app.handleIntentJson(fn);
+      return true;
+    }
+
+    /*
+     * Sets up the directory structure of the project.
+     * */
     setup(done) {
       const SETUP_DIRECTORIES = ['app/actions', 'app/middleware'];
-      for(let i=0; i < SETUP_DIRECTORIES.length; i++) {
+      for (let i = 0; i < SETUP_DIRECTORIES.length; i++) {
         try {
           thorin.util.fs.ensureDirSync(path.normalize(thorin.root + '/' + SETUP_DIRECTORIES[i]));
-        } catch(e) {}
+        } catch (e) {
+        }
       }
       done();
     }
 
     /*
-    * Runs the HTTP Server and binds it to the port.
-    * */
+     * Runs the HTTP Server and binds it to the port.
+     * */
     run(done) {
       this.app.listen((e) => {
-        if(e) return done(e);
+        if (e) return done(e);
         thorin.dispatcher.registerTransport(this);
         done();
       });
     }
+
     /* Manually add an express middleware. */
     addMiddleware(fn) {
-      if(!this[app]) {
+      if (!this[app]) {
         this[middleware].push(fn);
         return this;
       }
-      if(this.app.running) {
+      if (this.app.running) {
         console.error('Thorin.transport.http: addMiddleware() must be called before the app is running.');
         return this;
       }
-      if(typeof fn !== 'function') {
+      if (typeof fn !== 'function') {
         console.error('Thorin.transport.http: addMiddleware(fn) must be called with a function.');
         return this;
       }
@@ -109,14 +132,14 @@ module.exports = function init(thorin) {
     }
 
     /*
-    * Registers an incoming intent action.
-    * HTTP Actions work with aliases.
-    * */
+     * Registers an incoming intent action.
+     * HTTP Actions work with aliases.
+     * */
     routeAction(actionObj) {
       this.app.addHandler(actionObj);
-      for(let i=0; i < actionObj.aliases.length; i++) {
+      for (let i = 0; i < actionObj.aliases.length; i++) {
         let alias = actionObj.aliases[i];
-        if(typeof alias.verb !== 'string') {
+        if (typeof alias.verb !== 'string') {
           continue;
         }
         this.app.addHandler(actionObj, alias.verb, alias.name);
@@ -124,8 +147,8 @@ module.exports = function init(thorin) {
     }
 
     /*
-    * Temporary disable the action from being processed.
-    * */
+     * Temporary disable the action from being processed.
+     * */
     disableAction(actionName) {
       this.app.disableHandler(actionName);
       return this;
@@ -138,10 +161,10 @@ module.exports = function init(thorin) {
     }
 
     /*
-    * This will handle the transport logger.
-    * */
+     * This will handle the transport logger.
+     * */
     _log() {
-      if(this[config].debug === false) return;
+      if (this[config].debug === false) return;
       let logObj = thorin.logger(this.name);
       logObj.log.apply(logObj, arguments);
     }
